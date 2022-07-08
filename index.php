6cc5282eb1948f1287df3afaacbba7d097359ead
unknown <pgimsong@yahoo.com>
2022-07-08 19:01:40 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include 'navbar.php';
+?>
+
+<div class="container">
+    <marquee>
+        <h1 style='color:red'> ยินดีต้อนรับเข้าสู่เว็บไซต์ </h1>
+    </marquee>
+</div>
+
+<?php
+  include "footer.php";
+?>
